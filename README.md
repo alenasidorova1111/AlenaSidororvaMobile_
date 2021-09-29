9d1ba29cc3d1601513e2f956e1f8dd3e800428eb
Alena Sidorova <sidsidsid1111@gmail.com>
2021-09-29 17:27:51 +0300

    Added: DataProviders.java EPAMTestApp.apk README.md
    Updated:
    BaseTest.java
    IDriver.java
    IPageObject.java
    NativeMobileTests.java
    NativePageObject.java
    nativeTNG.xml
    PageObject.java
    pom.xml
    WebMobileTests.java
    WebPageObject.java
    webTNG.xml

---
+++ README.md
@@ -0,0 +1,14 @@
+## This project performs:
+- native android application test
+- web android application test
+
+###### _* The app is included_
+
+**To run tests you need to satisfy conditions below:**
+- run AVD emulator
+- run Appium server
+- run maven project build
+
+All parameters for Appium driver set in two .xml files in test/resources package.
+
+###### _* Project tests passed successfully on emulator device - Nexus 5X 5.2" 1080x1920 420 dpi Api27_
--- src/main/java/pageObjects/WebPageObject.java
+++ src/main/java/pageObjects/WebPageObject.java
@@ -1,14 +1,21 @@
 package pageObjects;
 
 import io.appium.java_client.AppiumDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
-public class WebPageObject  {
+import java.util.List;
 
-    public WebPageObject(AppiumDriver appiumDriver) {
+public class WebPageObject {
+
+    @FindBy(xpath = "//input[@name='q']")
+    private WebElement searchField;
+    @FindBy(xpath = "//div[@id='rso']/div")
+    List<WebElement> searchResults;
+
+    public WebPageObject(AppiumDriver<? extends WebElement> appiumDriver) {
         PageFactory.initElements(appiumDriver, this);
-
     }
-
 
 }
